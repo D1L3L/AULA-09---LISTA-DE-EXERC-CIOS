1dc1817f28252aac34044c67e91dc0155b048d28
Dani Oliveira <86899014+D1L3L@users.noreply.github.com>
2023-10-15 12:31:36 -0300

    ex03.py finalizado

--- ex03.py
+++ ex03.py
@@ -2,3 +2,29 @@
 #a) o valor do maior elemento de Q e a respectiva posição que ele ocupa no vetor;
 #b) o valor do menor elemento de Q e a respectiva posição que ele ocupa no vetor;
 
+def ler_numero_positivo():
+    while True:
+        try:
+            numero = int(input("Digite um número positivo: "))
+            if numero > 0:
+                return numero
+            else:
+                print("Por favor, digite um número positivo.")
+        except ValueError:
+            print("Entrada inválida. Por favor, digite um número válido.")
+
+Q = [0] * 5
+
+for i in range(5):
+    Q[i] = ler_numero_positivo()
+print (Q)
+
+maior_elemento = max(Q)
+posicao_maior_elemento = Q.index(maior_elemento)
+
+menor_elemento = min(Q)
+posicao_menor_elemento = Q.index(menor_elemento)
+
+print(f'\n A matriz contém {len(Q)} vetores.')
+print(f'\nMaior elemento da matriz Q: {maior_elemento}, na posição: {posicao_maior_elemento+1}')
+print(f'\nMenor elemento da matriz Q: {menor_elemento}, na posição: {posicao_menor_elemento+1}')
